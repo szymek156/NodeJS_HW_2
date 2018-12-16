5d1236c2b48079fbf26ccc5c098994947231e338
Szymon Zimnowoda <szymon.zimnowoda@gmail.com>
2018-12-16 23:16:33 +0100

    Finished

--- index.js
+++ index.js
@@ -37,77 +37,7 @@ const TestUser   = require("./tests/test_user");
 const TestToken  = require("./tests/test_token");
 const TestCart   = require("./tests/test_cart");
 
-let runner = new TestRunner([/*TestServer, TestUser, TestToken,*/ TestCart]);
+let runner = new TestRunner([TestServer, TestUser, TestToken, TestCart]);
 
-let exitNodeAfterFinish = true;
+let exitNodeAfterFinish = false;
 runner.runAll(exitNodeAfterFinish);
-
-
-
-// let https           = require("https");
-// const StringDecoder = require("string_decoder").StringDecoder;
-// var querystring     = require("querystring");
-
-// function callback(val) {
-//     console.log(val);
-// }
-
-// let order = {
-//     amount: 999,
-//     currency: "usd",
-//     description: "Example charge",
-//     source: "tok_visa"
-
-// };
-
-// let stringPayload = querystring.stringify(order);
-
-// var requestDetails = {
-//     "protocol": "https:",
-//     "hostname": "api.stripe.com",
-//     "method": "POST",
-//     "path": "/v1/charges",
-//     "auth": "sk_test_4eC39HqLyjWDarjtT1zdp7dc",
-//     "headers": {
-//         //"Authorization": "Basic c2tfdGVzdF80ZUMzOUhxTHlqV0Rhcmp0VDF6ZHA3ZGM6",
-//         "Content-Length": Buffer.byteLength(stringPayload),
-//         "User-Agent": "curl / 7.47.0",
-//         "Accept": "*/*",
-//         "Content-Type": "application/x-www-form-urlencoded"
-//     }
-// };
-
-
-// var req = https.request(requestDetails, function(res) {
-//     // Grab the status of the sent request
-//     var status = res.statusCode;
-//     // Callback successfully if the request went through
-
-//     let decoder = new StringDecoder("utf-8");
-//     let buffer  = "";
-
-//     res.on("data", function(chunk) {
-//         buffer += decoder.write(chunk);
-//     });
-//     res.on("end", function() {
-//         buffer += decoder.end();
-//         callback(buffer);
-//     });
-
-//     if (status == 200 || status == 201) {
-//         callback(false);
-//     } else {
-//         callback("Status code returned was " + status);
-//     }
-// });
-
-// // Bind to the error event so it doesn't get thrown
-// req.on("error", function(e) {
-//     callback(e);
-// });
-
-// // Add the payload
-// req.write(stringPayload);
-
-// // End the request
-// req.end();
